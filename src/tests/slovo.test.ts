9154525039dd0a70f233a47ae00e53fa9ce86b08
AndrewKushnir <cuckop30@gmail.com>
2024-06-04 16:36:04 +0300

    module9-task1

---
+++ src/tests/slovo.test.ts
@@ -0,0 +1,6 @@
+describe('Function: isArtistAnswerCorrect', () => {
+  it('should return "true" when answer is correct', () => {
+    const result = true;
+    expect(result).toBe(true);
+  });
+});
